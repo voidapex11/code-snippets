0c2c4f3eda6280ee321a1e103d615e7891eb35a1
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-05-17 10:20:11 +0100

    Update logging.py

--- Python/logging.py
+++ Python/logging.py
@@ -4,6 +4,7 @@ FORMAT = '%(asctime)s %(name)s %(levelname)s:\n\t%(message)s'
 logging.basicConfig(
     format=FORMAT,
     level=logging.INFO,
-    #datefmt='%d/%b/%y %H:%M:%S.%f'
 )
-logger = logging.getLogger('encoding')
+
+#example context:
+#logger = logging.getLogger('encoding')
