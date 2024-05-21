ee1d1b23b2a97e59f943489aa507f9b0daded391
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-05-21 15:49:27 +0100

    Update logger.py

--- Python/logger.py
+++ Python/logger.py
@@ -1,14 +1,13 @@
 import logging
 
-FORMAT = '%(asctime)s %(name)s %(levelname)s:\n\t%(message)s\n'# logging format
+FORMAT = '%(asctime)s %(name)s:%(process)d %(levelname)s:\n\t%(message)s\n'  # logging format
 logging.basicConfig(
     format=FORMAT,
-    level=logging.INFO,# set to DEBUG to show debug mesages
+    level=logging.INFO,  # set to DEBUG to show debug mesages
     handlers=[
-        logging.FileHandler("app.log"),# writes to file and console
+        logging.FileHandler("app.log"),  # writes to file and console
         logging.StreamHandler()
-    ]
-)
+    ])
 
 # example context:
 #logger = logging.getLogger('')
