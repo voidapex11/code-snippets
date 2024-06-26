20a130c9a01dfe7b982cd2e1210fd282325fdc0e
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-06-26 10:29:53 +0100

    security update for mathIt.py
    - prevented code execution in the program
    - made the code friendly to being imported by other programs

--- Python/mathIt.py
+++ Python/mathIt.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import cmath
 
 parser = argparse.ArgumentParser(
     prog="CalcIt",
@@ -8,27 +9,73 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-  "equation",
-  type=str,
-  help="The equation to be solved",
+    "equation",
+    type=str,
+    help="The equation to be solved",
 )
 
 args = parser.parse_args()
 
-def calcIt(data:str):
+
+def mathIt(data: str):
   try:
-    data=eval(data,{"math": math}, {})
+    data = eval(data, {"math": math, "cmath": cmath}, {})
     return data
   except Exception as e:
-    return "ERROR:",e
+    return "ERROR:", e
 
-ans=calcIt(args.equation)
-match isinstance(ans,tuple):
-  case True:
-    print(ans[0],ans[1])
-    break
-  case False:
-    print(ans)
-    break
-  case _:
-    print("wtf (error)") # ;)
+
+# this is a function so that it is import frendly
+def calcIt(equation: str, doPrint=True, doReturn=False):
+
+  risk = [
+      "import", "exec", "eval", "os", "sys", "subprocess", "global", "requests"
+  ]
+
+  # prevent use of high risk keywords
+  for i in risk:
+    if i in equation:
+      if doPrint:
+        print("ERROR: Risk of code execution detected, command canceled")
+      if doReturn:
+        return "ERROR: Risk of code execution detected, command canceled"
+      else:
+        return -2
+
+  # prevent use of python keywords to prevent posible threat actor problems
+  # lower risk though
+  for i in __builtins__.__dict__:
+    if i in equation:
+      if doPrint:
+        print("Python keyword Found, comand canceled")
+      if doReturn:
+        return "Python keyword Found, comand canceled"
+      else:
+        return -1
+
+  # prevent gits from using __builtins__ as a bypass
+  if "__builtins__" in equation:
+    if doPrint:
+      print("Python keyword Found, comand canceled")
+    if doReturn:
+      return "Python keyword Found, comand canceled"
+    else:
+      return -1
+
+  ans = mathIt(equation)
+  match isinstance(ans, tuple):
+    case True:
+      if doPrint:
+        print(ans[0], ans[1])
+      if doReturn:
+        return (ans[0], ans[1])
+    case False:
+      if doPrint:
+        print(ans)
+      if doReturn:
+        return ans
+    case _:
+      pass  #print("wtf (error)")  ;)
+
+
+calcIt(args.equation)
