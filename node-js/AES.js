b7c4fcf17b6642d6a9ed6c731aafce421ebe7f3f
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-07-04 09:35:43 +0100

    Create AES.js

---
+++ node-js/AES.js
@@ -0,0 +1,36 @@
+const { randomBytes, createCipheriv, createDecipheriv} = require('crypto');
+
+class AESCipher {
+  constructor() {
+    this.key = randomBytes(32); // 256-bit key
+    this.iv = randomBytes(16); // Initialization vector
+  }
+
+  encrypt(text) {
+    const cipher = createCipheriv('aes-256-cbc', this.key, this.iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+  }
+
+  decrypt(encryptedData) {
+    const decipher = createDecipheriv('aes-256-cbc', this.key, this.iv);
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+  }
+}
+
+function example(){
+  const myCipher = new AESCipher();
+  const text = 'Hello World!';
+  const encrypted = myCipher.encrypt(text);
+  console.log(myCipher)
+  console.log('Encrypted:', encrypted);
+
+  const decrypted = myCipher.decrypt(encrypted);
+  console.log('Decrypted:', decrypted);
+  console.log(myCipher)
+}
+
+//example()
