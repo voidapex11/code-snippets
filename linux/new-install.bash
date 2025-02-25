f2217e4fd7388df5c07d957c6fbd4577b6b18441
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2025-02-25 13:37:06 +0000

    add gping to newinstall.bash

--- linux/new-install.bash
+++ linux/new-install.bash
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # apt installs
-sudo apt install vim cmatrix zsh git net-tools zip unzip ufw python3 python3-pip tmux gnupg -y
+sudo apt install vim htop cmatrix zsh git net-tools zip unzip ufw python3 python3-pip tmux gnupg -y
 
 # install my zshrc ( as its my install scrip)
 wget https://github.com/voidapex11/code-snippets/blob/main/linux/.zshrc
@@ -24,6 +24,13 @@ VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -o
 wget https://github.com/$REPO/releases/download/$VERSION/fastfetch-linux-aarch64.deb
 sudo dpkg -i fastfetch-linux-aarch64.deb
 rm fastfetch-linux-aarch64.deb
+
+# install gping
+echo 'deb [signed-by=/usr/share/keyrings/azlux.gpg] https://packages.azlux.fr/debian/ bookworm main' | sudo tee /etc/apt/sources.list.d/azlux.list
+sudo apt install gpg
+curl -s https://azlux.fr/repo.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/azlux.gpg > /dev/null
+sudo apt update
+sudo apt install gping
 
 # curses for pinentry in gpg
 echo you want curses pinentry not the default
