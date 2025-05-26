0d249af9ab14029cd6ea6dee7be4ab17b3e2df59
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2025-05-26 11:29:25 +0100

    new file:   linux/install-nvim.bash new file:   linux/update-zshrc.bash

---
+++ linux/install-nvim.bash
@@ -0,0 +1,9 @@
+sudo apt-get install git cmake
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build
+cpack -G DEB
+sudo dpkg -i nvim-linux64.deb
+cd ../..
+rm -rf neovim
